73f9d94b455190130646ab070eb5a60cd5333bb4
ch1kulya <164895536+ch1kulya@users.noreply.github.com>
2026-01-04 16:36:19 +0300

    Add `data_issues` db view

---
+++ migrations/000012_data_issues_view.down.sql
@@ -0,0 +1,6 @@
+DROP VIEW IF EXISTS data_issues;
+DROP INDEX IF EXISTS idx_chapters_short_content;
+DROP INDEX IF EXISTS idx_chapters_empty_title;
+DROP INDEX IF EXISTS idx_novels_empty_title;
+DROP INDEX IF EXISTS idx_novels_empty_description;
+DROP INDEX IF EXISTS idx_novels_no_cover;
