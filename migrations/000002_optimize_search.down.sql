c6b8dfa6a4c88e75579f0c203e4399b47ce963aa
ch1kulya <164895536+ch1kulya@users.noreply.github.com>
2026-01-16 16:32:27 +0300

    Add missing down migrations

---
+++ migrations/000002_optimize_search.down.sql
@@ -0,0 +1,15 @@
+DROP INDEX IF EXISTS idx_novels_trgm_search;
+
+ALTER TABLE novels
+    DROP COLUMN IF EXISTS title_norm,
+    DROP COLUMN IF EXISTS title_en_norm,
+    DROP COLUMN IF EXISTS author_norm;
+
+CREATE INDEX IF NOT EXISTS idx_novels_title_norm_trgm_gin
+    ON novels USING gin (lower(regexp_replace(title, '[^a-zA-Z0-9]', '', 'g')) gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS idx_novels_title_en_norm_trgm_gin
+    ON novels USING gin (lower(regexp_replace(title_en, '[^a-zA-Z0-9]', '', 'g')) gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS idx_novels_author_norm_trgm_gin
+    ON novels USING gin (lower(regexp_replace(author, '[^a-zA-Z0-9]', '', 'g')) gin_trgm_ops);
---
+++ migrations/000004_add_sort_indexes.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_novels_title_cyrillic;
+DROP INDEX IF EXISTS idx_novels_created_at_sort;
+DROP INDEX IF EXISTS idx_novels_year_title;
