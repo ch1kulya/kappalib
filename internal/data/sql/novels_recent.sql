e873616d2b4043544662e1a433e620737d3154a0
ch1ka <164895536+ch1kulya@users.noreply.github.com>
2026-08-24 13:44:52 +0300

    feat: remove filters from updates

--- internal/data/sql/novels_recent.sql
+++ internal/data/sql/novels_recent.sql
@@ -8,12 +8,5 @@ SELECT
     cover_url,
     created_at
 FROM novels
-WHERE
-    NOT (
-        has_self_harm
-        OR has_drug_usage
-        OR has_sexual_violence
-        OR has_graphic_sex
-    )
 ORDER BY created_at DESC
 LIMIT NULLIF($1, 0);
