5c393cf70f78b6a1a179ed304dd8a404422480fd
harryys <gniceso225@gmail.com>
2026-09-01 23:20:42 +0700

    fix: fix to synchronize with the actual data (#177)

---
+++ migrations/000047_fix_source_stats.down.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER IF EXISTS trg_chapter_stats_ai ON chapters;
+DROP TRIGGER IF EXISTS trg_chapter_stats_ad ON chapters;
+DROP TRIGGER IF EXISTS trg_chapter_stats_au ON chapters;
+
+DROP FUNCTION IF EXISTS update_source_chapter_chapter_stats();
+
